e1c864fa95da75e2e2af7884ee1fca31541b2859
OSWorX <info@osworx.net>
2019-05-14 15:50:53 +0200

    Add files via upload
    German files for OC 2.x

---
+++ upload/admin/language/de-DE/extension/facebookadsextension.php
@@ -0,0 +1,21 @@
+<?php
+// Copyright 2017-present, Facebook, Inc.
+// All rights reserved.
+
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+
+/**
+ * German translation by https://osworx.net
+ */
+
+$_['heading_title']						= 'Facebook Werbung';
+$_['button_get_started']				= 'Jetzt beginnen';
+$_['button_manage_settings']			= 'Einstellungen';
+$_['sub_heading_title']					= 'Produkt in Werbung auf facebook umwandeln';
+$_['body_text']							= 'Einfach ein Pixel installieren und einen Produktkatakog auf facebook erstellen. Das Pixel verwenden um gezielt Kunden anzusprechen und den Wert der WErbungen zu messen.<br>Produkte einfach auf einmal bewerben anstatt diverse Werbungen zu erstellen.';
+$_['resync_text']						= 'Produkte mit facebook abgleichen';
+$_['resync_confirm_text']				= 'Produkte werden automatisch bei Neuerstellungen oder Aktualisierungen abgeglichen.<br>Soll jetzt wirklich eine manuelle Aktualisierung durchgeführt werden? Es werden dann alle veröffentlichten Produkte aktualisiert .. das kann einige Zeit dauern!<br>Dieser Vorgang wird nur dann benötigt wenn Produkte nicht aktuell sein sollten.';
+$_['download_log_file_text']			= 'Bericht downladen';
+$_['download_log_file_error_warning']	= 'Achtung: die Berichtsdatei %s ist %s groß!';
+$_['enable_cookie_bar_text']			= 'Zeige Cookiebar im Shop';
