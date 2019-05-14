b2cf0bc2ffb359053a1ae29d1b378b4cfb0d230c
OSWorX <info@osworx.net>
2019-05-14 15:48:17 +0200

    Update facebookadsextension.php

--- upload/admin/language/de-de/extension/facebookadsextension.php
+++ upload/admin/language/de-de/extension/facebookadsextension.php
@@ -12,10 +12,10 @@
 $_['heading_title']                   = 'Facebook Werbung';
 $_['button_get_started']              = 'Jetzt beginnen';
 $_['button_manage_settings']          = 'Einstellungen';
-$_['sub_heading_title']					      = 'Produkt in Werbung auf facebook umwandeln';
-$_['body_text']				    		        = 'Einfach ein Pixel installieren und einen Produktkatakog auf facebook erstellen. Das Pixel verwenden um gezielt Kunden anzusprechen und den Wert der WErbungen zu messen.<br>Produkte einfach auf einmal bewerben anstatt diverse Werbungen zu erstellen.';
-$_['resync_text']				      		    = 'Produkte mit facebook abgleichen';
-$_['resync_confirm_text']	      			= 'Produkte werden automatisch bei Neuerstellungen oder Aktualisierungen abgeglichen.<br>Soll jetzt wirklich eine manuelle Aktualisierung durchgeführt werden? Es werden dann alle veröffentlichten Produkte aktualisiert .. das kann einige Zeit dauern!<br>Dieser Vorgang wird nur dann benötigt wenn Produkte nicht aktuell sein sollten.';
-$_['download_log_file_text']			    = 'Bericht downladen';
+$_['sub_heading_title']               = 'Produkt in Werbung auf facebook umwandeln';
+$_['body_text']                       = 'Einfach ein Pixel installieren und einen Produktkatakog auf facebook erstellen. Das Pixel verwenden um gezielt Kunden anzusprechen und den Wert der WErbungen zu messen.<br>Produkte einfach auf einmal bewerben anstatt diverse Werbungen zu erstellen.';
+$_['resync_text']                     = 'Produkte mit facebook abgleichen';
+$_['resync_confirm_text']             = 'Produkte werden automatisch bei Neuerstellungen oder Aktualisierungen abgeglichen.<br>Soll jetzt wirklich eine manuelle Aktualisierung durchgeführt werden? Es werden dann alle veröffentlichten Produkte aktualisiert .. das kann einige Zeit dauern!<br>Dieser Vorgang wird nur dann benötigt wenn Produkte nicht aktuell sein sollten.';
+$_['download_log_file_text']          = 'Bericht downladen';
 $_['download_log_file_error_warning'] = 'Achtung: die Berichtsdatei %s ist %s groß!';
-$_['enable_cookie_bar_text']			    = 'Zeige Cookiebar im Shop';
+$_['enable_cookie_bar_text']          = 'Zeige Cookiebar im Shop';
